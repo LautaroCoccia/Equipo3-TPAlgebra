e8ecbdfc2fc4e9781ce590ef10c599422d81c57d
Lautaro Coccia <lautaroauditore@gmail.com>
2019-10-25 13:04:29 -0300

    Add ball movement

---
+++ Equipo3-TPAlgebra/Assets/Scripts/BallCollision.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    // public GameObject whiteBall;
+    private float posX;
+    private float posY;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.5f;
+        posX = 0.0f;
+        posY = 0.0f;
+        transform.position = new Vector2(posX, posY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.UpArrow))
+        {
+            posY += speed;
+            transform.position = new Vector2(posX, posY);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            posY -= speed;
+            transform.position = new Vector2(posX, posY);
+        } 
+
+       if (Input.GetKey(KeyCode.RightArrow))
+        {
+            posX += speed;
+            transform.position = new Vector2(posX, posY);
+        }
+       else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            posX -= speed;
+            transform.position = new Vector2(posX, posY);
+        }
+    }
+}
