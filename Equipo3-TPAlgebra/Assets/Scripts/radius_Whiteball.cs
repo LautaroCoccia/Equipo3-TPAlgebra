f45affee958251106bfcd75a04e12bfa4b3664ca
Facundo <mrfacu125@gmail.com>
2019-11-04 22:32:18 -0300

    Added scripts for the radius of all the balls.
    (And another table for the game)

---
+++ Equipo3-TPAlgebra/Assets/Scripts/radius_Whiteball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radius_Whiteball : MonoBehaviour
+{
+    //Objects
+
+    public GameObject white_ball;
+    public GameObject point_extreme;
+
+    //Vriables
+
+    public float white_ball_x;
+    public float white_ball_y;
+    public float point_extreme_x;
+    public float point_extreme_y;
+    public float white_ball_radius;
+  
+    //Functions
+
+    void Update()
+    {
+        calcRad();
+    }
+
+    void calcRad()
+    {
+
+        white_ball_x = white_ball.gameObject.transform.position.x;
+        white_ball_y = white_ball.gameObject.transform.position.y;
+        point_extreme_x = point_extreme.gameObject.transform.position.x;
+        point_extreme_y = point_extreme.gameObject.transform.position.y;
+
+        white_ball_radius = (white_ball_x + white_ball_y) - (point_extreme_x + point_extreme_y);
+
+        white_ball_radius = Mathf.Abs(white_ball_radius);
+
+    }
+
+}
---
+++ Equipo3-TPAlgebra/Assets/Scripts/radius_otherBalls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radius_otherBalls : MonoBehaviour
+{
+    //Objects
+
+    public GameObject other_ball;
+    public GameObject point_extreme;
+
+    //Variables
+
+    public float other_ball_x;
+    public float other_ball_y;
+    public float point_extreme_x;
+    public float point_extreme_y;
+    public float other_ball_radius;
+
+    //Functions
+
+    void Update()
+    {
+        calcRad();
+    }
+
+    void calcRad()
+    {
+
+        other_ball_x = other_ball.gameObject.transform.position.x;
+        other_ball_y = other_ball.gameObject.transform.position.y;
+        point_extreme_x = point_extreme.gameObject.transform.position.x;
+        point_extreme_y = point_extreme.gameObject.transform.position.y;
+
+        other_ball_radius = (other_ball_x + other_ball_y) - (point_extreme_x + point_extreme_y);
+
+        other_ball_radius = Mathf.Abs(other_ball_radius);
+
+    }
+
+}
