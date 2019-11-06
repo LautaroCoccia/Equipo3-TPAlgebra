7131a1dedab079109f036989497deddb02374f76
Lautaro Coccia <lautaroauditore@gmail.com>
2019-11-06 19:31:34 -0300

    Initial ball's pos script

---
+++ Equipo3-TPAlgebra/Assets/Scripts/make_Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class make_Triangle : MonoBehaviour
+{
+    public GameObject triangle;
+    public GameObject[] balls;
+    const int BALLCANT = 16;
+    public float ballRadius = 2.555f;
+
+
+    void Start()
+    {
+        balls[1].transform.position = new Vector2(-(ballRadius*2), 0);
+        for(int i =2; i< BALLCANT; i++)
+        {
+            for (int j = 2; j < BALLCANT; j++)
+            {
+            }
+            balls[i].transform.position = new Vector2(, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
