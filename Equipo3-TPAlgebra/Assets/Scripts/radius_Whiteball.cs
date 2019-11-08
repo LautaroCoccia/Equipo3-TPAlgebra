05f792c1d446fd557fe934d8b2620d20ae5e56a7
julianbega <julian.begamilrad@gmail.com>
2019-11-08 15:27:51 -0300

    La pelota se  mueve a la posision del mouse al clickear y oculta el palo

--- Equipo3-TPAlgebra/Assets/Scripts/radius_Whiteball.cs
+++ Equipo3-TPAlgebra/Assets/Scripts/radius_Whiteball.cs
@@ -8,6 +8,8 @@ public class radius_Whiteball : MonoBehaviour
 
     public GameObject white_ball;
     public GameObject point_extreme;
+    public GameObject cue;
+    public Camera camera;
 
     //Vriables
 
@@ -16,13 +18,44 @@ public class radius_Whiteball : MonoBehaviour
     public float point_extreme_x;
     public float point_extreme_y;
     public float white_ball_radius;
+    public bool AlreadyClick = false;
+    public bool isMoving = false;
+    public float speed = 200;
+    public Vector3 MousePos;
+
   
     //Functions
 
     void Update()
     {
-        calcRad();
-    }
+      //  calcRad();
+        if (isMoving == false)
+        {
+            MousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            MousePos.z = 45;
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            
+            AlreadyClick = true;
+            cue.gameObject.SetActive(false);
+
+
+        }
+        if (AlreadyClick == true)
+        {
+            isMoving = true;
+            this.transform.position = Vector3.MoveTowards(this.transform.position, MousePos, speed * Time.deltaTime);
+        }
+        if (this.transform.position == MousePos)
+        {
+            isMoving = false;
+            AlreadyClick = false;
+            cue.gameObject.SetActive(true);
+        }
+        
+    }   
 
     void calcRad()
     {
@@ -32,9 +65,9 @@ public class radius_Whiteball : MonoBehaviour
         point_extreme_x = point_extreme.gameObject.transform.position.x;
         point_extreme_y = point_extreme.gameObject.transform.position.y;
 
-        white_ball_radius = (white_ball_x + white_ball_y) - (point_extreme_x + point_extreme_y);
+        //white_ball_radius = (white_ball_x + white_ball_y) - (point_extreme_x + point_extreme_y);
 
-        white_ball_radius = Mathf.Abs(white_ball_radius);
+        //white_ball_radius = Mathf.Abs(white_ball_radius);
 
     }
 
