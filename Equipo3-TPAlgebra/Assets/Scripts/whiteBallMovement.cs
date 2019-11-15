4381f249f274384a688b3e39bf0991ba4b7db655
julianbega <julian.begamilrad@gmail.com>
2019-11-15 12:16:14 -0300

    detecta colisiones

--- Equipo3-TPAlgebra/Assets/Scripts/whiteBallMovement.cs
+++ Equipo3-TPAlgebra/Assets/Scripts/whiteBallMovement.cs
@@ -10,8 +10,10 @@ public class whiteBallMovement : MonoBehaviour
     public GameObject cue;
     public GameObject leftCorner;
     public GameObject rightCorner;
-    public GameObject topCorner;
-    public GameObject bottomCorner;
+    public GameObject topCorner1;
+    public GameObject bottomCorner1;
+    public GameObject topCorner2;
+    public GameObject bottomCorner2;
     public Camera camera;
 
     //Vriables
@@ -35,6 +37,8 @@ public class whiteBallMovement : MonoBehaviour
     public float invertVector = -1;
     Vector2 aux = new Vector2();
     float maxDistanceSpeed = 100.0f;
+    public bool changeSideX = false;
+    public bool changeSideY = false;
 
     public Animator cueAnim;
 
@@ -61,14 +65,27 @@ public class whiteBallMovement : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
+
             cueAnim.SetBool("IsOnCharge", false);
             
             MousePos = (Vector2)(camera.ScreenToWorldPoint(Input.mousePosition));
             directionTarget = (Vector2)(MousePos - transform.position);
             directionTarget.Normalize();
-            StartCoroutine(BallMovement(directionTarget));
-            StartCoroutine(DecreaseRoceForce(roceForce));
-
+           // if (changeSideX == false || changeSideY == false)
+            {
+                StartCoroutine(BallMovement(directionTarget));
+                StartCoroutine(DecreaseRoceForce(roceForce));
+            }
+            /*if (changeSideY == false && changeSideX == true)
+            {
+                StartCoroutine(BallMovementNegX(directionTarget));
+                StartCoroutine(DecreaseRoceForce(roceForce));
+            }
+            if (changeSideY == true && changeSideX == false)
+            {
+                StartCoroutine(BallMovementNegY(directionTarget));
+                StartCoroutine(DecreaseRoceForce(roceForce));
+            }*/
         }
         if (Input.GetKey(KeyCode.Mouse0) && distanceSpeed <= maxDistanceSpeed)
         {
@@ -76,84 +93,18 @@ public class whiteBallMovement : MonoBehaviour
             distanceSpeed += 0.2f;
         }
 
-        /*
-
-        if (isMoving == false)
+        if ((this.transform.position.x - whiteBallRadius) <= leftCorner.transform.position.x || (this.transform.position.x + whiteBallRadius) >= rightCorner.transform.position.x)
         {
-            MousePos = camera.ScreenToWorldPoint(Input.mousePosition);
-            MousePos.z = 45;
+          //  changeSideX = !changeSideX;
+            Debug.Log("Chocó en los costados");
+        }
+
+        if ((this.transform.position.y - whiteBallRadius) <= bottomCorner1.transform.position.y || (this.transform.position.y - whiteBallRadius) <= bottomCorner2.transform.position.y || (this.transform.position.y + whiteBallRadius) >= topCorner1.transform.position.y || (this.transform.position.y + whiteBallRadius) >= topCorner2.transform.position.y)
+        {
+           // changeSideY = !changeSideY;
+            Debug.Log("Chocó arriba o abajo");
         }
         
-        if (Input.GetMouseButtonUp(0))
-        {
-            
-            AlreadyClick = true;
-
-            directionTarget = MousePos;
-
-            oppositeDirection = (invertVector * directionTarget);
-            oppositeDirection.z = 45;
-
-            cue.gameObject.SetActive(false);
-
-        }
-        if (AlreadyClick == true)
-        {
-            isMoving = true;
-            whiteBall.transform.position = Vector3.MoveTowards(whiteBall.transform.position, directionTarget, vel );
-        }
-        if (hasCrash == true)
-        {
-            whiteBall.transform.position = Vector3.MoveTowards(whiteBall.transform.position, oppositeDirection, vel);
-        }
-
-        
-        if (whiteBall.transform.position == directionTarget ||whiteBall.transform.position == oppositeDirection)
-        {
-            isMoving = false;
-            AlreadyClick = false;
-            hasCrash = false;
-            cue.gameObject.SetActive(true);
-        }
-
-        //Colision no RANCIA (Other Script)
-
-        //Colision RANCIA
-
-        if ((whiteBallX - whiteBallRadius) <= leftCorner.transform.position.x || (whiteBallX + whiteBallRadius) >= rightCorner.transform.position.x)
-        {
-
-            isMoving = false;
-            AlreadyClick = false;
-            hasCrash = true;
-
-            if (whiteBall.transform.position == oppositeDirection)
-            {
-                cue.gameObject.SetActive(true);
-            }
-            else if (whiteBall.transform.position != oppositeDirection)
-            {
-                cue.gameObject.SetActive(false);
-            }
-
-        }
-
-        if ((whiteBallY - whiteBallRadius) <= bottomCorner.transform.position.y || (whiteBallY + whiteBallRadius) >= topCorner.transform.position.y)
-        {
-            isMoving = false;
-            AlreadyClick = false;
-            hasCrash = true;
-
-            if (whiteBall.transform.position == oppositeDirection)
-            {
-                cue.gameObject.SetActive(true);
-            }
-            else if (whiteBall.transform.position != oppositeDirection)
-            {
-                cue.gameObject.SetActive(false);
-            }
-
-        }*/
     }
 
     void calcNewDirection()
@@ -202,4 +153,53 @@ public class whiteBallMovement : MonoBehaviour
         return rForce;
     }
 
+    /*
+    IEnumerator BallMovementNegX(Vector3 _direction)
+    {
+        cueAnim.SetBool("IsOnDischarge", true);
+
+        Debug.Log("llegó");
+        yield return new WaitForSeconds(0.25f);
+
+        cue.gameObject.SetActive(false);
+
+        for (int i = 0; i < 25; i++)
+        {
+            transform.position += (_direction * (vel + (restRoceForce(roceForce))));
+            
+            if (i == 24)
+            {
+                cueAnim.SetBool("IsOnDischarge", false);
+                cue.gameObject.SetActive(true);
+            }
+            _direction.x = _direction.x * invertVector;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+
+    IEnumerator BallMovementNegY(Vector3 _direction)
+    {
+        cueAnim.SetBool("IsOnDischarge", true);
+
+        Debug.Log("llegó");
+        yield return new WaitForSeconds(0.25f);
+
+        cue.gameObject.SetActive(false);
+
+        for (int i = 0; i < 25; i++)
+        {
+            transform.position += (_direction * (vel + (restRoceForce(roceForce))));
+
+            if (i == 24)
+            {
+                cueAnim.SetBool("IsOnDischarge", false);
+                cue.gameObject.SetActive(true);
+            }
+            _direction.y = _direction.y * invertVector;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+    */
 }
